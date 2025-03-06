a33d38efdab822b4846bb811129d854101b0fdae
tayyabmushtaq15 <tayyabmushtaq15@gmail.com>
2025-03-06 15:47:30 +0500

    Initialize Project and Folder Structure

---
+++ src/app/lib/placeholder.data.js
@@ -0,0 +1,39 @@
+const posts = [
+    {
+        id: "1234344343",
+        title: "Hello, this is the first title",
+        content: "This is the first title content",
+        date: "276761",
+        user: "Test User 1"
+    },
+    {
+        id: "2234344344",
+        title: "Exploring the Future of AI",
+        content: "AI is evolving rapidly and changing the world.",
+        date: "276762",
+        user: "Test User 2"
+    },
+    {
+        id: "3234344345",
+        title: "Understanding JavaScript Closures",
+        content: "Closures allow functions to access variables from an outer scope.",
+        date: "276763",
+        user: "Test User 3"
+    },
+    {
+        id: "4234344346",
+        title: "Why Next.js is the Future of Web Development",
+        content: "Next.js offers server-side rendering, static site generation, and much more.",
+        date: "276764",
+        user: "Test User 4"
+    },
+    {
+        id: "5234344347",
+        title: "The Importance of Mental Health",
+        content: "Taking care of mental health is as important as physical health.",
+        date: "276765",
+        user: "Test User 5"
+    }
+];
+
+console.log(posts);
